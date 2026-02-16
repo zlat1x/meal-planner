5bd82188c49e7c7dcc43a1aacaaaec8814c9e7b8
Zlata Dovbyk <dovbykzlata@gmail.com>
2026-02-16 05:48:06 +0200

    add db connection console project

---
+++ app/MealPlanner.DbConnect/Program.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+
+var connectionString =
+    "Host=localhost;Port=5434;Database=meal_planner;Username=meal_user;Password=meal_pass";
+
+try
+{
+    await using var conn = new NpgsqlConnection(connectionString);
+    await conn.OpenAsync();
+
+    await using (var cmd = new NpgsqlCommand("SELECT COUNT(*) FROM users;", conn))
+    {
+        var count = (long)(await cmd.ExecuteScalarAsync() ?? 0);
+        Console.WriteLine($"Connected users count = {count}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("DB connection error");
+    Console.WriteLine(ex.Message);
+}
